85071674a31a21a454479b7a4e73eb997f470677
Aymen Al-Quaiti <aalquaiti@gmail.com>
2020-08-10 00:06:49 +1000

    Updates io files

--- src/main/java/me/aymen/anes/io/IO.java
+++ src/main/java/me/aymen/anes/io/IO.java
@@ -1,11 +1,14 @@
 package me.aymen.anes.io;
 
 /**
- * Describes IO devices connected to bus and is accessed by CPU
+ * Describes IO devices connected to bus and is accessed by CPU or PPU
  */
-public enum  CPUIO {
+public enum IO {
     RAM,
     PPU_IO,
     APU_IO,
-    PGR_ROM
+    PGR_ROM,
+    CHR_ROM,
+    VRAM,
+    PLTE // PPU Palette Memory,
 }
--- src/main/java/me/aymen/anes/io/VRAM.java
+++ src/main/java/me/aymen/anes/io/VRAM.java
@@ -1,2 +1,13 @@
-package me.aymen.anes.io;public class VRAM {
+package me.aymen.anes.io;
+
+/**
+ * Represents Video Memory used by PPU
+ */
+public class VRAM {
+    public final static int SIZE = 0x800;
+    protected final int memory[];
+
+    public VRAM() {
+        memory = new int[SIZE];
+    }
 }
